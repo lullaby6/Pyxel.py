a0f148fc49c5bf42c0af69452ac8bb451537a8f8
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-17 16:49:26 -0300

    game, gameobject, bg_color and alpha, pause

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-from pyclassgame import Game, Scene, GameObject
+from pyclassgame import Game, Scene, GameObject, Colors
 
 class MainScene (Scene):
-    def __init__(self, name, game):
-        super().__init__(name, game)
-    def update(self):
-        self.game.screen.fill((125,0,255))
+    def __init__(self):
+        super().__init__()
+
     def key_down(self, event, key_name):
-        print(event.key, key_name)
+        # print(event.key, key_name)
+        if key_name == 'f': self.game.toggle_fullscreen()
+        elif key_name == 'p': self.game.toggle_pause()
 
 class Player (GameObject):
-    def __init__(self, name, scene):
-        super().__init__(name, scene)
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
+        self.alpha = 100
+
     def update(self):
+        self.scene.game.camera.target(self.x, self.y)
         pass
+        # print('xd')
+
+    def key_down(self, event, keyname):
+        if keyname == 'w': self.y -= 10
+        if keyname == 'a': self.x -= 10
+        if keyname == 's': self.y += 10
+        if keyname == 'd': self.x += 10
 
 if __name__ == "__main__":
-    game = Game(640, 480, 'Title')
+    game = Game(640, 480, Colors['lightblue'], 'Title')
 
-    game.add_scene('main', MainScene)
-    game.change_scene('main')
-    game.get_active_scene().add_game_object('player', Player)
+    game.set_scene('main', MainScene())
+    game.get_active_scene().add_game_object('player', Player(color=Colors['red']))
+    game.get_active_scene().instant_game_object(GameObject())
 
     game.run()
--- pyclassgame.py
+++ pyclassgame.py
@@ -1,10 +1,38 @@
 import pygame, sys, json, uuid
 
+Colors = {
+    'white': (255, 255, 255),
+    'black': (0, 0, 0),
+    'red': (255, 0, 0),
+    'green': (0, 255, 0),
+    'blue': (0, 0, 255),
+    'yellow': (255, 255, 0),
+    'purple': (255, 0, 255),
+    'orange': (255, 165, 0),
+    'cyan': (0, 255, 255),
+    'brown': (165, 42, 42),
+    'pink': (255, 192, 203),
+    'lightgray': (211, 211, 211),
+    'lightblue': (173, 216, 230),
+    'lightgreen': (144, 238, 144),
+}
+
 class GameObject:
-    def __init__(self, name = None, scene = None):
-        self.name = name
-        self.scene = scene
-        self.tags = []
+    def __init__(self, x = 0, y = 0, width = 10, height = 10, color = Colors['white'], tags = []):
+        self.name = None
+        self.scene = None
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.alpha = 255
+        self.tags = tags
+    def drawing(self):
+        self.surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+        self.surface.set_alpha(self.alpha)
+        self.surface.fill(self.color)
+        self.scene.game.screen.blit(self.surface, (self.x - self.scene.game.camera.x, self.y - self.scene.game.camera.y))
     def add_tag(self, tag):
         self.tags.append(tag)
         return tag
@@ -16,42 +44,60 @@ class GameObject:
     def get_tags(self):
         return self.tags
 
-class Scene:
-    def __init__(self, name = None, game = None):
-        self.name = name
+class Camera:
+    def __init__(self, game, x = 0, y = 0):
         self.game = game
+        self.x = x
+        self.y = y
+    def target(self, x, y):
+        self.x = x - (self.game.width / 2)
+        self.y = y - (self.game.height / 2)
+
+class Scene:
+    def __init__(self):
+        self.name = None
+        self.game = None
         self.game_objects = {}
     def add_game_object(self, name, game_object):
-        self.game_objects[name] = game_object(name, self)
+        self.game_objects[name] = game_object
+        self.game_objects[name].name = name
+        self.game_objects[name].scene = self
         return self.game_objects[name]
     def instant_game_object(self, game_object):
-        name = str(uuid.uuid4())
-        self.game_objects[name] = game_object(name, self)
-        return self.game_objects[name]
+        random_uuid_name = str(uuid.uuid4())
+        self.add_game_object(random_uuid_name, game_object)
     def remove_game_object(self, name):
         del self.game_objects[name]
     def get_game_object(self, name):
         return self.game_objects[name]
 
 class Game:
-    def __init__(self, width = 640, height = 480, title = 'Title', fps = 60, default_scene = Scene):
+    def __init__(self, width = 640, height = 480, bg_color = Colors['black'], title = 'Title', fps = 60, default_scene = Scene()):
         pygame.init()
         self.set_title(title)
         self.width = width
         self.height = height
-        self.screen = pygame.display.set_mode((width, height))
+        self.bg_color = bg_color
+        self.bg_alpha = 255
+        self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
         self.fps = fps
         self.prev_time = pygame.time.get_ticks()
 
         self.scenes = {}
         self.active_scene = None
-        self.add_scene('default', default_scene)
-        self.change_scene('default')
+        self.set_scene('default', default_scene)
 
         self.quit_on_escape = False
+        self.fullscreen = False
+
+        self.camera = Camera(self, 0, 0)
+
+        self.running = False
+        self.pause = False
     def run(self):
-        while True:
+        self.running = True
+        while self.running:
             active_scene = self.get_active_scene()
             game_objects = active_scene.game_objects
             events = {
@@ -63,22 +109,28 @@ class Game:
                 'mouse_wheel': pygame.MOUSEWHEEL,
                 'joy_axis_motion': pygame.JOYAXISMOTION,
                 'joy_button_down': pygame.JOYBUTTONDOWN,
-                'joy_button_up': pygame.JOYBUTTONUP
+                'joy_button_up': pygame.JOYBUTTONUP,
+                'quit': pygame.QUIT,
+                'fullscreen': pygame.FULLSCREEN,
+                'resize': pygame.VIDEORESIZE,
+                'expose': pygame.VIDEOEXPOSE,
+                'focus': pygame.ACTIVEEVENT,
             }
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    return
+                    self.running = False
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE and self.quit_on_escape:
-                        return
+                        self.running = False
                 for eventName in events:
                     if event.type == events[eventName]:
                         key_name = None
                         if hasattr(event, 'key'): key_name = pygame.key.name(event.key)
                         if hasattr(active_scene, eventName):
                             getattr(active_scene, eventName)(event, key_name)
-                        for game_object in game_objects:
+                        for game_object_name in game_objects:
+                            game_object = game_objects[game_object_name]
                             if hasattr(game_object, eventName):
                                 getattr(game_object, eventName)(event, key_name)
 
@@ -86,21 +138,52 @@ class Game:
             self.delta_time = (current_time - self.prev_time) / 1000.0
             self.prev_time = current_time
 
-            if hasattr(active_scene, 'update'): active_scene.update()
-            for game_object in game_objects:
-                if hasattr(game_object, 'update'): game_object.update()
+            self.bg_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
+            self.bg_surface.set_alpha(self.bg_alpha)
+            self.bg_surface.fill(self.bg_color)
+            self.screen.blit(self.bg_surface, (0, 0))
 
+            if hasattr(active_scene, 'update') and self.pause == False: active_scene.update()
+            if hasattr(active_scene, 'draw'): active_scene.draw()
+            for game_object_name in game_objects:
+                game_object = game_objects[game_object_name]
+                if hasattr(game_object, 'update') and self.pause == False: game_object.update()
+                game_object.drawing()
+                if hasattr(game_object, 'draw'): game_object.draw()
+
+            pygame.display.flip()
             pygame.display.update()
             self.clock.tick(self.fps)
     def set_title(self, title):
         self.title = title
         pygame.display.set_caption(title)
     def add_scene(self, name, scene):
-        self.scenes[name] = scene(name, self)
+        self.scenes[name] = scene
+        self.scenes[name].name = name
+        self.scenes[name].game = self
         return self.scenes[name]
     def change_scene(self, name):
         self.active_scene = name
+    def set_scene(self, name, scene):
+        self.add_scene(name, scene)
+        self.change_scene(name)
     def get_active_scene(self):
         return self.scenes[self.active_scene]
     def remove_scene(self, name):
         del self.scenes[name]
+    def set_fullscreen(self, fullscreen = True):
+        self.fullscreen = fullscreen
+        if fullscreen == True:
+            self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
+        else:
+            self.screen = pygame.display.set_mode((self.width, self.height))
+    def toggle_fullscreen(self):
+        self.set_fullscreen(not self.fullscreen)
+    def set_size(self, width, height):
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+    def set_pause(self, pause):
+        self.pause = pause
+    def toggle_pause(self):
+        self.set_pause(not self.pause)
